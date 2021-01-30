589854e6e48fd2fa904f1f9ed652e4c20a09fb4b
Alvaro Marques <alvaromarques@guitarra.nics.unicamp.br>
2021-01-30 20:45:18 -0300

    Adding comments on load_dataset

--- test_transfer_cnn_seg/load_dataset.py
+++ test_transfer_cnn_seg/load_dataset.py
@@ -1,12 +1,15 @@
 import os
-
+# Desabilitando warnings do tensorflow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-import glob
 import tensorflow as tf
+
+# Mostrando estado do tensorflow
 print("TensorFlow version: {}".format(tf.__version__))
 print("Eager execution: {}".format(tf.executing_eagerly()))
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
 from pickle import load, dump
 import pandas as pd
 import numpy as np
@@ -15,36 +18,34 @@ import dcase_util
 from tensorflow.keras.models import load_model, save_model
 from sklearn.preprocessing import OneHotEncoder
 
-path = "features"
+### Criacao do dataset
 
+# Constantes relativas aos diretorios e arquivos utilizados
 FEATURES_DIR = "features"
 BASE_DIR = "/pub/dcase/datasets/datasets/TAU-Segmentado/TAU-urban-acoustic-scenes-2020-3class-development"
 CSV = "/evaluation_setup/fold1_train.csv"
 CSV_TEST = "/evaluation_setup/fold1_evaluate.csv"
+fold_stats_filename = "norm_fold_1.cpickle"
 
 def pathname(name):
     return BASE_DIR + "/" + name
 
 def change(name):
-    return os.path.join(path, name.replace("/", "-").replace("wav","cpickle"))
+    return os.path.join(FEATURES_DIR, name.replace("/", "-").replace("wav","cpickle"))
 
-#def load_pickle()
+train = pd.read_csv(pathname(CSV), sep='\t')
 
-meta = pd.read_csv(pathname(CSV), sep='\t')
-SIZE = meta.values.shape[0]
+SIZE = train.values.shape[0]
 
-filenames = np.asarray([
-    change(name) for name in meta['filename'].values
-])
-
-fold_stats_filename = "norm_fold_1.cpickle"
+# Importando o normalizador usado no baseline
 normalizer = dcase_util.data.Normalizer().load(filename=fold_stats_filename)
 
+# Transforando as labels no format de one hot
 encoder = OneHotEncoder()
 
-encoder.fit(meta['scene_label'].values.reshape(-1, 1))
+encoder.fit(train['scene_label'].values.reshape(-1, 1))
 
-labels_ = encoder.transform(meta['scene_label'].values.reshape(-1, 1)).toarray()
+labels_ = encoder.transform(train['scene_label'].values.reshape(-1, 1)).toarray()
 
 test = pd.read_csv(pathname(CSV_TEST), sep='\t')
 
@@ -53,10 +54,8 @@ labels_test = encoder.transform(test['scene_label'].values.reshape(-1, 1)).toarr
 with open("one_hot.cpickle", "wb") as file_ohe:
     dump(encoder, file_ohe)
 
-p
-
 def generator_train():
-    for x, y in zip(meta['filename'].values, labels_):
+    for x, y in zip(train['filename'].values, labels_):
         with open(change(x), 'rb') as file_open:
             array = load(file_open)
             yield normalizer.normalize(array).reshape((40,26,1)), y
@@ -103,11 +102,8 @@ loss_object = tf.keras.losses.CategoricalCrossentropy(
     name='categorical_crossentropy'
 )
 
-#init = tf.compat.v1.initialize_all_variables() 
-
 def loss(model, x, y, training):
-  # training=training is needed only if there are layers with different
-  # behavior during training versus inference (e.g. Dropout).
+  # Se calcula o loss por categorical crossentropy
   y_ = model(x, training=training)
 
   return loss_object(y_true=y, y_pred=y_)
@@ -117,45 +113,44 @@ def grad(model, inputs, targets):
     loss_value = loss(model, inputs, targets, training=True)
   return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
+# Otimizador utilizado
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
 num_epochs = 30
 
 train_dataset = dataset
 
-for _ in enumerate(dataset_test):
-    break
-
+# Looop de treinamento principal
 for epoch in range(num_epochs):
-  print(epoch)
   epoch_loss_avg = tf.keras.metrics.Mean()
   epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
   epoch_val_loss_avg = tf.keras.metrics.Mean()
   epoch_val_accuracy = tf.keras.metrics.CategoricalAccuracy()
 
-  # Training loop - using batches of 64
+  # Treinamento
   for i, (x, y) in enumerate(dataset):
     # Optimize the model
     loss_value, grads = grad(model, x, y)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-    # Track progress
-    epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-    # Compare predicted label to actual label
-    # training=True is needed only if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
+    # Adicionando os valores de loss de cada iteracao
+    epoch_loss_avg.update_state(loss_value) 
+
+    # Adicionando mais valores para o calculo da acuracia da epoca
     epoch_accuracy.update_state(y, model(x, training=True))
 
+  # Validacao
   for i, (x, y) in enumerate(dataset_test):
+    # Calcula-se o loss com treinamento desabilitado
     loss_value = loss(model, x, y, training=False)
     epoch_val_loss_avg.update_state(loss_value)
     epoch_val_accuracy.update_state(y, model(x, training=False))
         
-
-  print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+  print("Epoch {:03d}:")
+  print("Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
                                                                 epoch_loss_avg.result(),
                                                                 epoch_accuracy.result()))
-  print("Epoch {:03d}: Val Loss: {:.3f}, Val Accuracy: {:.3%}".format(epoch,
+  print("Val Loss: {:.3f}, Val Accuracy: {:.3%}".format(epoch,
                                                                 epoch_val_loss_avg.result(),
                                                                 epoch_val_accuracy.result()))
 

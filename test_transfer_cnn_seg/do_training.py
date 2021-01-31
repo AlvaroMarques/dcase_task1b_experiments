7e4c65477bc48147e4962aa8ef7f3d4aae8f0cc4
Alvaro Marques <alvaromarques@guitarra.nics.unicamp.br>
2021-01-31 17:56:35 -0300

    Using tabs instead of spaces

--- test_transfer_cnn_seg/do_training.py
+++ test_transfer_cnn_seg/do_training.py
@@ -27,11 +27,15 @@ CSV = "/evaluation_setup/fold1_train.csv"
 CSV_TEST = "/evaluation_setup/fold1_evaluate.csv"
 fold_stats_filename = "norm_fold_1.cpickle"
 
+MODEL_INPUT = "model_train_5.h5"
+MODEL_OUTPUT = "final_output.h5"
+STATE_MODEL_PATTERN = "model_train_"
+
 def pathname(name):
-    return BASE_DIR + "/" + name
+		return BASE_DIR + "/" + name
 
 def change(name):
-    return os.path.join(FEATURES_DIR, name.replace("/", "-").replace("wav","cpickle"))
+		return os.path.join(FEATURES_DIR, name.replace("/", "-").replace("wav","cpickle"))
 
 train = pd.read_csv(pathname(CSV), sep='\t')
 
@@ -52,106 +56,106 @@ test = pd.read_csv(pathname(CSV_TEST), sep='\t')
 labels_test = encoder.transform(test['scene_label'].values.reshape(-1, 1)).toarray()
 
 with open("one_hot.cpickle", "wb") as file_ohe:
-    dump(encoder, file_ohe)
+		dump(encoder, file_ohe)
 
 def generator_train():
-    for x, y in zip(train['filename'].values, labels_):
-        with open(change(x), 'rb') as file_open:
-            array = load(file_open)
-            yield normalizer.normalize(array).reshape((40,26,1)), y
+		for x, y in zip(train['filename'].values, labels_):
+				with open(change(x), 'rb') as file_open:
+						array = load(file_open)
+						yield normalizer.normalize(array).reshape((40,26,1)), y
 
 def generator_test():
-    for x, y in zip(test['filename'].values, labels_test):
-        with open(change(x), 'rb') as file_open:
-            array = load(file_open)
-            yield normalizer.normalize(array).reshape((40, 26, 1)), y
+		for x, y in zip(test['filename'].values, labels_test):
+				with open(change(x), 'rb') as file_open:
+						array = load(file_open)
+						yield normalizer.normalize(array).reshape((40, 26, 1)), y
 
-dataset_test = tf.data.Dataset.from_generator(generator_test,
-                                            output_types=(
-                                                tf.float32,
-                                                tf.float32
-                                                ),
-                                           output_shapes=(
-                                                tf.TensorShape([40,26,1]),
-                                                tf.TensorShape([3]) 
-                                               )
-                                           ).batch(1024)
+dataset_test=tf.data.Dataset.from_generator(generator_test,
+    output_types=(
+        tf.float32,
+        tf.float32
+    ),
+    output_shapes=(
+        tf.TensorShape([40,26,1]),
+        tf.TensorShape([3])
+    )
+).batch(1024)
 
 dataset = tf.data.Dataset.from_generator(generator_train,
-                                            output_types=(
-                                                tf.float32,
-                                                tf.float32
-                                                ),
-                                           output_shapes=(
-                                                tf.TensorShape([40,26,1]),
-                                                tf.TensorShape([3]) 
-                                               )
-                                           ).batch(1024)
+    output_types=(
+        tf.float32,
+        tf.float32
+    ),
+    output_shapes=(
+        tf.TensorShape([40,26,1]),
+        tf.TensorShape([3])
+    )
+).batch(1024)
+
+
 
 features, labels = next(iter(dataset))
-    
 
-#iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
-#el = iterator.get_next()
-
-model = load_model("model_transfer.h5")
+model = load_model(MODEL_INPUT)
 print(model.summary())
 
 loss_object = tf.keras.losses.CategoricalCrossentropy(
-    from_logits=False, label_smoothing=0,
-    name='categorical_crossentropy'
+		from_logits=False, label_smoothing=0,
+		name='categorical_crossentropy'
 )
 
 def loss(model, x, y, training):
-  # Se calcula o loss por categorical crossentropy
-  y_ = model(x, training=training)
-
-  return loss_object(y_true=y, y_pred=y_)
+	# Se calcula o loss por categorical crossentropy
+	y_ = model(x, training=training)
+	return loss_object(y_true=y, y_pred=y_)
 
 def grad(model, inputs, targets):
-  with tf.GradientTape() as tape:
-    loss_value = loss(model, inputs, targets, training=True)
-  return loss_value, tape.gradient(loss_value, model.trainable_variables)
+	with tf.GradientTape() as tape:
+		loss_value = loss(model, inputs, targets, training=True)
+	return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
 # Otimizador utilizado
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
-
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 num_epochs = 30
 
 train_dataset = dataset
 
+metrics = []
+
 # Looop de treinamento principal
 for epoch in range(num_epochs):
-  epoch_loss_avg = tf.keras.metrics.Mean()
-  epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
-  epoch_val_loss_avg = tf.keras.metrics.Mean()
-  epoch_val_accuracy = tf.keras.metrics.CategoricalAccuracy()
+	if epoch % 5 == 0:
+	 save_model(model, "{}_{}.h5".format(STATE_MODEL_PATTERN, epoch))
+	epoch_loss_avg = tf.keras.metrics.Mean()
+	epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
+	epoch_val_loss_avg = tf.keras.metrics.Mean()
+	epoch_val_accuracy = tf.keras.metrics.CategoricalAccuracy()
 
-  # Treinamento
-  for i, (x, y) in enumerate(dataset):
-    # Optimize the model
-    loss_value, grads = grad(model, x, y)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+	# Treinamento
+	for i, (x, y) in enumerate(dataset):
+		# Optimize the model
+		loss_value, grads = grad(model, x, y)
+		optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-    # Adicionando os valores de loss de cada iteracao
-    epoch_loss_avg.update_state(loss_value) 
+		# Adicionando os valores de loss de cada iteracao
+		epoch_loss_avg.update_state(loss_value) 
 
-    # Adicionando mais valores para o calculo da acuracia da epoca
-    epoch_accuracy.update_state(y, model(x, training=True))
+		# Adicionando mais valores para o calculo da acuracia da epoca
+		epoch_accuracy.update_state(y, model(x, training=True))
+	# Validacao
+	for i, (x, y) in enumerate(dataset_test):
+		# Calcula-se o loss com treinamento desabilitado
+		loss_value = loss(model, x, y, training=False)
+		epoch_val_loss_avg.update_state(loss_value)
+		epoch_val_accuracy.update_state(y, model(x, training=False))
+				
+	print("Epoch {:03d}:".format(epoch))
+	print("Loss: {:.3f}, Accuracy: {:.3%}".format(epoch_loss_avg.result(), epoch_accuracy.result()))
+	print("Val Loss: {:.3f}, Val Accuracy: {:.3%}".format(epoch_val_loss_avg.result(), epoch_val_accuracy.result()))
 
-  # Validacao
-  for i, (x, y) in enumerate(dataset_test):
-    # Calcula-se o loss com treinamento desabilitado
-    loss_value = loss(model, x, y, training=False)
-    epoch_val_loss_avg.update_state(loss_value)
-    epoch_val_accuracy.update_state(y, model(x, training=False))
-        
-  print("Epoch {:03d}:")
-  print("Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
-  print("Val Loss: {:.3f}, Val Accuracy: {:.3%}".format(epoch,
-                                                                epoch_val_loss_avg.result(),
-                                                                epoch_val_accuracy.result()))
+	metrics.append([epoch_loss_avg.result(), epoch_accuracy.result(), 
+			epoch_val_loss_avg.result(), epoch_val_accuracy.result()])
 
-save_model(model, "result2.h5")
+save_model(model, MODEL_OUTPUT)
+with open("metrics.csv","a") as metrics_file:
+		metrics_file.write("\n".join([",".join(map(lambda x: str(float(x)), l)) for l in metrics]))
